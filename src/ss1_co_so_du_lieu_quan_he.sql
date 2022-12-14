4b85c48fa66f2ed7fabcf825026fac741781f4d4
Nhan <thanhnhanphan2888@gmail.com>
2022-12-14 10:43:05 +0700

    bai tap xay dung csdl quan li sinh vien

---
+++ src/ss1_co_so_du_lieu_quan_he.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE student_management;
+CREATE TABLE class (
+    id int,
+    name varchar(45)
+    );
+CREATE TABLE Teacher(
+id int,
+name varchar(45),
+age int,
+country varchar(45)
+);
