8e945d779f4c86b7a1fddd0876dc2ea8d228f498
Nhan <thanhnhanphan2888@gmail.com>
2022-12-15 11:02:36 +0700

    task 1.2

--- case_study_database/building_table.sql
+++ case_study_database/building_table.sql
@@ -1,6 +1,7 @@
 -- Task 1: Thêm mới thông tin cho tất cả các bảng có trong CSDL để có thể thoả mãn các yêu cầu bên dưới.
 create database furama_resort_management;
 use furama_resort_management;
+
 create table vi_tri(
    ma_vi_tri int primary key,
    ten_vi_tri varchar(45)
@@ -26,8 +27,11 @@ create table nhan_vien(
    email varchar(45),
    dia_chi varchar(45),
    ma_tri_tri int,
+   foreign key (ma_tri_tri) references vi_tri(ma_vi_tri),
    ma_trinh_do int,
-   ma_bo_phan int
+   foreign key (ma_trinh_do) references trinh_do(ma_trinh_do),
+   ma_bo_phan int,
+   foreign key (ma_bo_phan) references bo_phan(ma_bo_phan)
 );
 
 create table loai_khach(
@@ -38,6 +42,7 @@ create table loai_khach(
 create table khach_hang(
    ma_khach_hang  int primary key,
    ma_loai_khach int,
+   foreign key(ma_loai_khach) references loai_khach(ma_loai_khach),
    ho_ten varchar(45),
    ngay_sinh date,
    gioi_tinh bit(1),
@@ -60,7 +65,9 @@ create table kieu_thue(
 create table dich_vu(
    ma_dich_vu int primary key,
    ma_kieu_thue int,
+   foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue),
    ma_loai_dich_vu int,
+   foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu),
    ten_dich_vu varchar(45),
    dientich int,
    chi_phi_thue double,
@@ -80,19 +87,25 @@ create table dich_vu_di_kem(
    trang_thai varchar(45)
 );
 
-create table hop_dong_chi_tiet(
-   ma_hop_dong_chi_tiet int primary key,
-   ma_dich_vu_di_kem int,
-   ma_hop_dong int,
-   so_luong int
-);
-
 create table hop_dong(
    ma_hop_dong int primary key,
    ngay_lam_hop_dong date,
    ngay_ket_thuc date,
    tien_dat_coc double,
    ma_nhan_vien int,
+   foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
    ma_khach_hang int,
-   ma_dich_vu int
+   foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
+   ma_dich_vu int,
+   foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
 );
+
+create table hop_dong_chi_tiet(
+   ma_hop_dong_chi_tiet int primary key,
+   ma_dich_vu_di_kem int,
+   foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
+   ma_hop_dong int,
+   foreign key (ma_hop_dong) references hop_dong(ma_hop_dong),
+   so_luong int
+);
+
