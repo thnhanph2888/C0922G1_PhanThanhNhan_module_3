cf7c8d1b5f0bc717c779da309c0181c719ea046c
Nhan <thanhnhanphan2888@gmail.com>
2022-12-16 17:18:30 +0700

    update

--- src/ss3_database_manipulation/excercise/manipulation_with_sales_management_database.sql
+++ src/ss3_database_manipulation/excercise/manipulation_with_sales_management_database.sql
@@ -26,16 +26,16 @@ insert into order_detail values (1, 1, 3),
 	(2, 5, 4),
 	(2, 3, 3);
 
- select o.order_id, o.order_date, o.order_total_price from this_order o;
+select o.order_id, o.order_date, o.order_total_price from this_order o;
 
- select distinct c.customer_name from customer c, this_order o where c.customer_id = o.customer_id;
+select distinct c.customer_name from customer c, this_order o where c.customer_id = o.customer_id;
  
- select distinct p.product_name from product p, order_detail de where p.product_id = de.product_id;
+select distinct p.product_name from product p, order_detail de where p.product_id = de.product_id;
 
 select distinct c.customer_name from customer c where c.customer_id not in (
     select distinct c.customer_id from customer c, this_order o where c.customer_id = o.customer_id
 );
 
- select distinct o.order_id, o.order_date, de.order_quality * p.product_price price  from this_order o join product p join order_detail de on o.order_id = de.order_id and de.product_id = p.product_id;
+select distinct o.order_id, o.order_date, de.order_quality * p.product_price price  from this_order o join product p join order_detail de on o.order_id = de.order_id and de.product_id = p.product_id;
 ;
  
